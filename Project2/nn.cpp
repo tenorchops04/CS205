d48ab9a62a12341b46dde5bf00df15188a5d1ee5
Celvin Lizama Pena <celvinlizama@aol.com>
2022-06-06 18:29:53 -0700

    Beginning work on project 2: Nearest Neighbor Classifier

---
+++ Project2/nn.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Welcome to Celvin Lizama Pena's Feature Selection Algorithm.\nType in the name of the file to test:\n";
+    string filename;
+    cin >> filename;
+
+    cout << "Type the number of the algorithm you want to run.\n1)\tForward Selection\n2)\tBackward Elimination\n";
+    int choice;
+    cin >> choice;
+
+}
